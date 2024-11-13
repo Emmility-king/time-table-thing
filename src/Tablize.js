e8d6bb04817586d8909afb20bc37a63f258d03cf
Olafisoye Emmanuel <85019193+Emmility-king@users.noreply.github.com>
2024-11-13 14:45:14 +0100

    Hope changes

--- src/Tablize.js
+++ src/Tablize.js
@@ -2,83 +2,91 @@ import React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Table } from 'react-bootstrap';
+  // import React from 'react'  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { Modal, Table } from 'react-bootstrap';
+    function Tablize(props) {
+        const classroomNames = ['Twin Lecture Hall', 'TMA hall', 'NLT hall', 'Hall N', 'Hall C'];
 
-function Tablize(props) {
+        let [day, setd] = React.useState(
+            Array.from({ length: 50 }, v => (
 
-    let [day, setd] = React.useState(
-        Array.from({ length: 50 }, v => (
-            Array.from({ length: 50 }, v => Array.from({ length: 50 }, v => { <div></div> })))));
+                Array.from({ length: 50 }, v => Array.from({ length: 50 }, v => <div></div>)))));
 
-    const datam = [];
-    const days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+        const datam = [];
+        const days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 
-    function back(i) {
-        day[i] = Array.from({ length: 50 }, v => Array.from({ length: 50 }, v => { <div></div> }));
-        for (let j = 0; j < props.no_of_days; j++) {
-            day[i][j].push(<td><center>{days[j]}</center></td>);
-            for (let k = 0; k < props.no_of_Lectures; k++) {
+        function back(i) {
 
-                if ((props.no_of_Lectures % 2) !== 0) {
-                    if (k === Math.floor((props.no_of_Lectures - 1) / 2)) {
-                        day[i][j].push(<td><center> --- </center></td>);
+            day[i] = Array.from({ length: 50 }, v => Array.from({ length: 50 }, v => <div></div>));
+            for (let j = 0; j < props.no_of_days; j++) {
+                day[i][j].push(<td><center>{days[j]}</center></td>);
+                for (let k = 0; k < props.no_of_Lectures; k++) {
+
+                    if ((props.no_of_Lectures % 2) !== 0) {
+                        if (k === Math.floor((props.no_of_Lectures - 1) / 2)) {
+                            day[i][j].push(<td><center> --- </center></td>);
+                        }
                     }
-                }
 
 
-                if (props.time_table[i][k][j] !== 0) {
-                    day[i][j].push(<td><center>{props.time_table[i][k][j]} <br /> {props.faculty_names[props.time_table[i][k][j] - 1]}</center></td>);
-                }
-                else
-                    day[i][j].push(<td><center></center></td>);
+                    if (props.time_table[i][k][j] !== 0) {
+                        day[i][j].push(<td><center>{props.time_table[i][k][j]} <br /> {props.faculty_names[props.time_table[i][k][j] - 1]}</center></td>);
+                    }
+                    else
+                        day[i][j].push(<td><center></center></td>);
+
+                  if (props.time_table[i][k][j] !== 0) {
+                      day[i][j].push(<td><center>{props.time_table[i][k][j]} <br /> {props.faculty_names[props.time_table[i][k][j] - 1]}</center></td>);
+                  }
+                  else
+                      day[i][j].push(<td><center></center></td>);
 
 
-
-                if ((props.no_of_Lectures % 2) === 0) {
-                    if (k === Math.floor((props.no_of_Lectures - 1) / 2)) {
-                        day[i][j].push(<td><center> --- </center></td>);
+                    if ((props.no_of_Lectures % 2) === 0) {
+                        if (k === Math.floor((props.no_of_Lectures - 1) / 2)) {
+                            day[i][j].push(<td><center> --- </center></td>);
+                        }
                     }
                 }
             }
         }
-    }
 
-    for (let i = 0; i < props.no_of_classes; i++) {
-        back(i);
-        datam.push(<div>
-            <h3>For Classroom {i + 1}</h3>
-            <Table striped bordered hover size="sm" responsive striped bordered hover variant="success">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        {props.data}
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {day[i][0]}
-                    </tr>
-                    <tr>
-                        {day[i][1]}
-                    </tr>
-                    <tr>
-                        {day[i][2]}
-                    </tr>
-                    <tr>
-                        {day[i][3]}
-                    </tr>
-                    <tr>
-                        {day[i][4]}
-                    </tr>
-                    <tr>
-                        {day[i][5]}
-                    </tr>
-                </tbody >
-            </Table >
-        </div>
-        );
-    }
-
-    return (
+        for (let i = 0; i < props.no_of_classes; i++) {
+            back(i);
+            datam.push(<div>
+                <h3>{classroomNames[i] || `Classroom ${i + 1}`}</h3>
+                <Table striped bordered hover size="sm" responsive striped bordered hover variant="success">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            {props.data}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {day[i][0]}
+                        </tr>
+                        <tr>
+                            {day[i][1]}
+                        </tr>
+                        <tr>
+                            {day[i][2]}
+                        </tr>
+                        <tr>
+                            {day[i][3]}
+                        </tr>
+                        <tr>
+                            {day[i][4]}
+                        </tr>
+                        <tr>
+                            {day[i][5]}
+                        </tr>
+                    </tbody >
+                </Table >
+            </div>
+            );
+        }
+  }    return (
         <div>
             <Modal
                 {...props}
@@ -103,5 +111,4 @@ function Tablize(props) {
         </div >
     )
 }
-
 export default Tablize
